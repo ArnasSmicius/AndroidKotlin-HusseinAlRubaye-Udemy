02f38f16340db2206fb32237dbcd17dd45fd3233
ArnasSmicius <arnasmic@gmail.com>
2018-02-25 11:21:21 +0200

    Nested For loop

---
+++ KotlinJava/KotlinUdemyDemo/src/NestedForLoop.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+
+    for (count1 in 1..10) {
+        println("Count1 = $count1")
+        for (count2 in 1..7) {
+            println("Count2 = $count2")
+        }
+        println("Loop count2 done")
+    }
+    println("Loop count1 done")
+}
