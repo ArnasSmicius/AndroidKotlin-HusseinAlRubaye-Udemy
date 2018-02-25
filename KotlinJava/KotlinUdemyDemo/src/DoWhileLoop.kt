0ad0328265f57b5916232262047f9bae8e26d843
ArnasSmicius <arnasmic@gmail.com>
2018-02-25 11:30:43 +0200

    Do While loop

---
+++ KotlinJava/KotlinUdemyDemo/src/DoWhileLoop.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+
+    var count = 11
+
+    // Do while loop, execute block of code at least one time
+    do {
+        println("Count = $count")
+        count++
+    } while (count <= 10)
+}
