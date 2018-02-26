c9bb74e21e3a7c7380d736eeda575e470f913252
ArnasSmicius <arnasmic@gmail.com>
2018-02-26 19:01:45 +0200

    Strings

---
+++ KotlinJava/KotlinUdemyDemo/src/Strings.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+
+    var title = "Welcome to New York"
+    println(title)
+    println("Title = " + title)
+    println("Title = $title")
+    var name = "Arnas " + "Smicius"
+    println("name = $name")
+    println("Second char = ${title[1]}")
+    println("title = " + title.toLowerCase())
+    println("title = " + title.toUpperCase())
+    println("title = " + title.split(" "))
+    println("title = " + title.trim())
+}
