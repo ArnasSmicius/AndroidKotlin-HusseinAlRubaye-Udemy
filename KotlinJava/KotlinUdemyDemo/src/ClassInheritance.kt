5e0558b7e6b7939412d21bb7a519f50033799f11
ArnasSmicius <arnasmic@gmail.com>
2018-03-02 19:14:04 +0200

    Inheritance

---
+++ KotlinJava/KotlinUdemyDemo/src/ClassInheritance.kt
@@ -0,0 +1,25 @@
+open class Operations {
+    fun sum(n1: Int, n2: Int): Int = n1 + n2
+
+    fun division(n1: Int, n2: Int): Int = n1 / n2
+}
+
+class MultiOperation: Operations() {
+    fun sub(n1: Int, n2: Int): Int = n1 - n2
+
+    fun mult(n1: Int, n2: Int): Int = n1 * n2
+}
+
+fun main(args: Array<String>) {
+    var op = Operations()
+    var sum = op.sum(10, 15)
+    println("Sum = $sum")
+    var div = op.division(12, 11)
+    println("Div = $div")
+
+    var op2 = MultiOperation()
+    sum = op2.sum(10, 15)
+    println("Sum = $sum")
+    div = op2.division(12, 11)
+    println("Div = $div")
+}
