7b2ca4b4e008453b0af3d28689ee548556855e18
ArnasSmicius <arnasmic@gmail.com>
2018-03-05 21:09:14 +0200

    Reading from file

--- KotlinJava/KotlinUdemyDemo/src/MyFIle.kt
+++ KotlinJava/KotlinUdemyDemo/src/MyFIle.kt
@@ -1,17 +1,30 @@
+import java.io.FileReader
 import java.io.FileWriter
 
 fun main(args: Array<String>) {
+    readFromFile()
     print("Write text to file: ")
-    var str = readLine().toString()
+    val str = readLine().toString()
     writeToFile(str)
 }
 
 fun writeToFile(str: String) {
     try {
-        var fo = FileWriter("test.txt", true)
+        val fo = FileWriter("test.txt", true)
         fo.write(str + "\n")
         fo.close()
     } catch (e: Exception) {
         println(e.message)
+    }
+}
+
+fun readFromFile() {
+    try {
+        val fin = FileReader("test.txt")
+        fin.forEachLine { println(it) }
+        fin.close()
+
+    } catch (ex: Exception) {
+        println(ex.message)
     }
 }
